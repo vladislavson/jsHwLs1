8089ceff10fd1549efe21de2a0efdc22d07c7c00
vladislavson <60482791+vladislavson@users.noreply.github.com>
2020-02-05 18:57:50 +0500

    prod version

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt('Введите своё имя');
+var age = prompt('Введите свой возраст');
+var solution1 = prompt('6 + 3 = ?')
+var solution2 = prompt('20 - 5 = ?')
+var solution3 = prompt('15 * 2 = ?')
+var solution4 = prompt('8 / 4 = ?')
+var solution5 = prompt('10 % 3 = ?')
+alert('Откройте консоль для просмотра')
+console.log('Меня зовут ' + name);
+console.log('Мне ' + age + ' лет');
+console.log('Пример 1: 6 + 3 = 9 Ваш ответ ' + solution1);
+console.log('Пример 2: 20 - 5 = 15 Ваш ответ ' + solution2);
+console.log('Пример 3: 15 * 2 = 30 Ваш ответ ' + solution3);
+console.log('Пример 4: 8 / 4 = 2 Ваш ответ ' + solution4);
+console.log('Пример 5: 10 % 3 = 1 Ваш ответ ' + solution5);
